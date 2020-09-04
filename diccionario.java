49d8cdb77e55dff44cbdc1ecfd21590fe3f5ae1a
JoseMiguelGonzalez098 <60407733+JoseMiguelGonzalez098@users.noreply.github.com>
2020-09-04 12:31:02 -0600

    Add files via upload

--- diccionario.java
+++ diccionario.java
@@ -1,79 +1,99 @@
-/******************************************************************
-biblioteca.java
-Autores: Jose Gonzalez, Carol Arevalo
-Última modificación: 19/08/20
-
-Biblioteca de palabras de español a cacchiquel para proyecto POO
-******************************************************************/
-
 import java.util.ArrayList;
 import java.util.List;
 
 class diccionario{
+
+    //el numero max de palabras que puede haber por diccionario, lo hago variable para alterarlo al agregar o quitar una palabra
+    public static int numespanol = 20;
+    public static int numkaq = 20;
+
+    //espanol, para las palabras en espanol.
+    //kaq, para las palabras en kaqchikel.
+    public static String[] espanol = new String[numespanol];
+    public static String[] kaq = new String[numkaq];
+
+    public static void quitar (String palespanol){
+
+        for(int i=0; i < numespanol; i++){
+
+            if(espanol[i].equals(palespanol)){
+
+                espanol[i] = null;
+
+            }
+
+        }
+
+    }
+
+    public static void agregar (String palespanol, String palkaq){
+
+        //le sumo para hacer dos cosas de una, uno cambiar el valor max de objetos en espanol y kaq y dos para la posicion del objeto
+        numespanol++;
+        numkaq++;
+
+        //agrego las palabras a espanol y a kaq
+        espanol[numespanol] = palespanol;
+        kaq[numkaq] = palkaq;
+
+    }
 
     public String biblioteca (int numero){
 
         //el numero, ay no jodan es para usar array
         int num = numero;
 
-        //espanol, para las palabras en espanol.
-        //kaq, para las palabras en kaqchikel.
-        String[] espanol = new String[20];
-        String[] kaq = new String[20];
-
         String respuesta = null;
 
         if(num < 20){
-            //Voc de saludos
-            espanol[0] = "Buenos días";
-            espanol[1] = "Gracias";
-            espanol[2] = "Buenas noches";
-            espanol[3] = "¿Cómo estas?";
-            espanol[4] = "Bien gracias";
 
-            //Voc de familia
-            espanol[5] = "Familia";
-            espanol[6] = "Madre";
-            espanol[7] = "Padre";
-            espanol[8] = "Hijos";
-            espanol[9] = "Nieto";
+            espanol[0] = "A";
+            espanol[1] = "I";
+            espanol[2] = "U";
+            espanol[3] = "E";
+            espanol[4] = "O";
 
-            //Voc de diario
-            espanol[10] = "¿Cuál es tu nombre?";
-            espanol[11] = "Dormitorio";
-            espanol[12] = "Sala";
-            espanol[13] = "Baño";
-            espanol[14] = "Cocina";
+            espanol[5] = "KA";
+            espanol[6] = "KI";
+            espanol[7] = "KU";
+            espanol[8] = "KE";
+            espanol[9] = "KO";
 
-            espanol[15] = "Cabeza";
-            espanol[16] = "Oreja";
-            espanol[17] = "Nariz";
-            espanol[18] = "Ojos";
-            espanol[19] = "Boca";
+            espanol[10] = "SA";
+            espanol[11] = "SHI";
+            espanol[12] = "SU";
+            espanol[13] = "SE";
+            espanol[14] = "SO";
 
-            kaq[0] = "Saqer";
-            kaq[1] = "Matyox";
-            kaq[2] = "Xo’qa";
-            kaq[3] = "La ütz awäch?”, ";
-            kaq[4] = "Ütz matyox";
+            espanol[15] = "TA";
+            espanol[16] = "CHI";
+            espanol[17] = "TSU";
+            espanol[18] = "TE";
+            espanol[19] = "TO";
 
-            kaq[5] = "Ach´alal";
-            kaq[6] = "Te´ej";
-            kaq[7] = "Tata´aj";
-            kaq[8] = "Alk´walaxela";
-            kaq[9]= "mam";
+            kaq[0] = "あ";
+            kaq[1] = "い";
+            kaq[2] = "う";
+            kaq[3] = "え";
+            kaq[4] = "お";
 
-            kaq[10] = "Achike ab’i’ rat?";
-            kaq[11] = "warab’äl";
-            kaq[12] = "nimajay";
-            kaq[13] = "ruxikin jay";
-            kaq[14] = "ruchi’ q’aq’";
+            kaq[5] = "か";
+            kaq[6] = "き";
+            kaq[7] = "く";
+            kaq[8] = "け";
+            kaq[9]= "こ";
 
-            kaq[15] = "jolomaj";
-            kaq[16] = "xikinaj";
-            kaq[17] = "tzamaj";
-            kaq[18] = "runaq’ wachaj";
-            kaq[19] = "chi’aj";
+            kaq[10] = "さ";
+            kaq[11] = "し";
+            kaq[12] = "す";
+            kaq[13] = "せ";
+            kaq[14] = "そ";
+
+            kaq[15] = "た";
+            kaq[16] = "ち";
+            kaq[17] = "つ";
+            kaq[18] = "て";
+            kaq[19] = "と";
 
 
             respuesta = espanol[num] + "," + kaq[num];
